1ea66c0ec787431c66a504bceb0866073fcbaae4
sprashan069 <sprashan069@gmail.com>
2019-05-14 15:55:47 +0530

    first commit

--- fabfile.py
+++ fabfile.py
@@ -24,7 +24,7 @@ def commit():
     local("git add -A")
     # local("git commit")
     local('git commit -a -m  "first commit" ', capture=False)
-    local("git remote add origin https://github.com/sprashan069/Fabric_test.git")
+    local("git remote add origin https://github.com/sprashan069/Fabric_test.git ")
 
 def push():
     local("git push -u origin master")
